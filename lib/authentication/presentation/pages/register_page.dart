5faa3e64c05eb57d3ced0cc5f1f4bc995eb2d10b
Doug Todd <doug.todd@hey.com>
2024-10-09 21:58:04 +0100

    Added register widget
    Added a widget for registering new users
    Also added a loading button that shows a spinner and gets disabled when
    it's loading

--- lib/authentication/presentation/pages/register_page.dart
+++ lib/authentication/presentation/pages/register_page.dart
@@ -4,7 +4,9 @@ import 'package:go_router/go_router.dart';
 import 'package:ink_relay/app_exception.dart';
 import 'package:ink_relay/authentication/presentation/controllers/register_controller.dart';
 import 'package:ink_relay/common/extensions/toastification_extensions.dart';
+import 'package:ink_relay/common/widgets/loading_button.dart';
 import 'package:ink_relay/routing/app_router.dart';
+import 'package:ink_relay/theme/theme.dart';
 import 'package:toastification/toastification.dart';
 
 class RegisterPage extends ConsumerWidget {
@@ -32,22 +34,77 @@ class RegisterPage extends ConsumerWidget {
 
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            FilledButton(
-              onPressed: () async {
-                await ref.read(registerControllerProvider.notifier).register();
-              },
-              child: const Text('Register'),
+        child: Card(
+          elevation: 5,
+          surfaceTintColor: context.theme.colorScheme.surfaceTint,
+          child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: FormFactor.mobile.breakpoint),
+            child: Padding(
+              padding: EdgeInsets.all(context.theme.appSpacing.veryLarge),
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Center(
+                      child: SizedBox(
+                        height: 75,
+                        width: 75,
+                        child: Placeholder(),
+                      ),
+                    ),
+                    SizedBox(height: context.theme.appSpacing.medium),
+                    Text(
+                      'Register',
+                      style: context.theme.textTheme.headlineMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: context.theme.appSpacing.small),
+                    Text(
+                      'Create an account to get started managing your clients and feel happier.',
+                      style: context.theme.textTheme.bodyMedium,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(height: context.theme.appSpacing.veryLarge),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      enabled: !ref.watch(registerControllerProvider).isLoading,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.email),
+                        labelText: 'Email',
+                        hintText: 'Enter your email',
+                      ),
+                    ),
+                    SizedBox(height: context.theme.appSpacing.medium),
+                    TextFormField(
+                      keyboardType: TextInputType.visiblePassword,
+                      enabled: !ref.watch(registerControllerProvider).isLoading,
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        labelText: 'Password',
+                        hintText: 'Enter your password',
+                      ),
+                    ),
+                    SizedBox(height: context.theme.appSpacing.veryLarge),
+                    LoadingButton(
+                      label: 'Register',
+                      icon: Icons.person_add,
+                      isLoading:
+                          ref.watch(registerControllerProvider).isLoading,
+                      onPressed: () async {
+                        await ref
+                            .read(registerControllerProvider.notifier)
+                            .register();
+                      },
+                    ),
+                    SizedBox(height: context.theme.appSpacing.veryLarge),
+                    Text('Already have an account? Login.'),
+                  ],
+                ),
+              ),
             ),
-            FilledButton(
-              onPressed: () {
-                context.goNamed(AppRoute.register.name);
-              },
-              child: const Text('Login'),
-            ),
-          ],
+          ),
         ),
       ),
     );
---
+++ lib/common/widgets/loading_button.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:ink_relay/theme/theme.dart';
+
+class LoadingButton extends StatelessWidget {
+  const LoadingButton({
+    required this.label,
+    required this.icon,
+    required this.isLoading,
+    required this.onPressed,
+    super.key,
+  });
+
+  final String label;
+  final IconData icon;
+  final bool isLoading;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton.icon(
+      onPressed: isLoading ? null : onPressed,
+      icon: isLoading
+          ? Container(
+              width: 24,
+              height: 24,
+              padding: const EdgeInsets.all(2),
+              child: CircularProgressIndicator(
+                strokeWidth: 3,
+                color: context.theme.colorScheme.onPrimary,
+              ),
+            )
+          : Icon(icon),
+      label: Text(label),
+    );
+  }
+}
